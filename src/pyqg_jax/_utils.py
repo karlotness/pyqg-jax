f260ead7c880daaad851a910c1e4db9da3f908b5
Karl Otness <karl.otness@nyu.edu>
2023-02-05 02:58:24 -0500

    Add module with internal utilities.
    These aren't user-facing and mostly just things to streamline our
    interactions with JAX.

---
+++ src/pyqg_jax/_utils.py
@@ -0,0 +1,51 @@
+# Copyright Karl Otness
+# SPDX-License-Identifier: MIT
+
+
+import typing
+import operator
+import dataclasses
+import jax
+import jaxtyping
+
+
+Children = typing.TypeVar("Children", bound=jaxtyping.PyTree)
+AuxData = typing.TypeVar("AuxData")
+
+
+class _PyTreePrivateProtocol(typing.Protocol[Children, AuxData]):
+    def _tree_flatten(self) -> typing.Tuple[Children, AuxData]:
+        ...
+
+    @classmethod
+    def _tree_unflatten(
+            cls: typing.Type[typing.Self],
+            aux_data: AuxData,
+            children: Children
+    ) -> typing.Self:
+        ...
+
+
+C = typing.TypeVar("C", bound=typing.Type[_PyTreePrivateProtocol])
+Class = typing.TypeVar("Class", bound=typing.Type)
+
+
+def register_pytree_node_class_private(cls: C) -> C:
+    jax.tree_util.register_pytree_node(
+        cls,
+        operator.methodcaller("_tree_flatten"),
+        cls._tree_unflatten,
+    )
+    return cls
+
+
+def register_pytree_dataclass(cls: Class) -> Class:
+    def flatten(obj):
+        fields = tuple(f.name for f in dataclasses.fields(cls))
+        return [getattr(obj, name) for name in fields], fields
+
+    def unflatten(aux_data, flat_contents):
+        return cls(**dict(zip(aux_data, flat_contents)))
+
+    jax.tree_util.register_pytree_node(cls, flatten, unflatten)
+    return cls
