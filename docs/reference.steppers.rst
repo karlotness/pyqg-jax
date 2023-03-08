0e9f868a549c9d6a049fe8986ec217ee7599bb17
Karl Otness <karl.otness@nyu.edu>
2023-03-08 03:53:56 -0500

    Add documentation for time stepping.

--- docs/reference.steppers.rst
+++ docs/reference.steppers.rst
@@ -3,10 +3,31 @@ Time-Steppers
 
 .. automodule:: pyqg_jax.steppers
 
-.. autoclass:: AB3Stepper
 
-.. autoclass:: StepperState
+Combined Models
+---------------
+
+These utilities add time-stepping to base models.
 
 .. autoclass:: SteppedModel
+   :members: create_initial_state, initialize_stepper_state, step_model, get_full_state
+
+.. autoclass:: StepperState
+   :members: update
+
+Time Stepping Schemes
+---------------------
+
+Implemented time-stepping schemes (currently only :class:`AB3Stepper`).
+
+.. autoclass:: AB3Stepper
+   :members: initialize_stepper_state, apply_updates
+   :inherited-members:
+
+State Manipulation
+------------------
+
+:class:`NoStepValue` makes it possible to shield values from
+time-stepping so they can be updated manually.
 
 .. autoclass:: NoStepValue
--- src/pyqg_jax/steppers.py
+++ src/pyqg_jax/steppers.py
@@ -2,6 +2,15 @@
 # SPDX-License-Identifier: MIT
 
 
+"""Time-stepping schemes and utilities for using them to update model states.
+
+To step a model through time, use :class:`SteppedModel`.
+
+:class:`AB3Stepper` implements the same time-stepping scheme used in
+PyQG.
+"""
+
+
 __all__ = ["SteppedModel", "AB3Stepper", "AB3State", "NoStepValue"]
 
 
@@ -18,6 +27,27 @@ P = typing.TypeVar("P", bound=jaxtyping.PyTree)
 
 @_utils.register_pytree_node_class_private
 class StepperState(typing.Generic[P]):
+    """Model state wrapped for time-stepping
+
+    Warning
+    -------
+    You should not construct this class yourself. Instead, you should
+    obtain instances from your chosen time stepper, or
+    :class:`SteppedModel`.
+
+    Attributes
+    ----------
+
+    state : PseudoSpectralState or ParameterizedModelState
+        The inner state from the model being stepped forward. The
+        actual type of `state` depends on the model being stepped.
+
+    t : jax.numpy.float32
+        The current model time
+
+    tc : jax.numpy.uint32
+        The current model timestep
+    """
     def __init__(self, *, state: P, t: float, tc: int):
         self.state = state
         self.t = jnp.float32(t)
@@ -35,6 +65,32 @@ class StepperState(typing.Generic[P]):
         return obj
 
     def update(self, **kwargs):
+        """Replace values stored in this state.
+
+        This function produces a *new* state object, containing the
+        replacement values.
+
+        The keyword arguments may be any of `state`, `t`, or `tc`.
+
+        The object this method is called on is not modified.
+
+        Parameters
+        ----------
+        state : PseudoSpectralState or ParameterizedModelState, optional
+            Replacement value for :attr:`state`.
+
+        t : jax.numpy.float32, optional
+            Replacement value for :attr:`t`.
+            The current model time
+
+        tc : jax.numpy.uint32, optional
+            Replacement value for :attr:`tc`.
+
+        Returns
+        -------
+        StepperState
+            A copy of this object with the specified values replaced.
+        """
         # Check that only valid updates are applied
         if not kwargs.keys() <= {"state", "t", "tc"}:
             raise ValueError("invalid state updates, can only update state, t, and tc")
@@ -65,18 +121,108 @@ class Stepper:
 
 @_utils.register_pytree_node_class_private
 class SteppedModel:
+    """Combine an inner model with a time stepper.
+
+    This class simplifies the process of stepping a base model through
+    time by handling the interactions between the model and time
+    stepper.
+
+    Parameters
+    ----------
+    model
+        The inner model to step through time.
+
+    stepper
+        The time stepper applying the updates to the model each step.
+
+    Attributes
+    ----------
+    model
+        The inner model being stepped.
+
+    stepper
+        The time stepper used to apply the updates.
+    """
+
     def __init__(self, model, stepper):
         self.model = model
         self.stepper = stepper
 
-    def create_initial_state(self, key):
-        model_state = self.model.create_initial_state(key=key)
+    def create_initial_state(self, key, *args, **kwargs):
+        """Create a new wrapped initial state with random
+        initialization.
+
+        This function defers to :attr:`model` to initialize the inner
+        state, passing it any additional arguments. Then wraps it by
+        calling `initialize_stepper_state` on :attr:`stepper`.
+
+        Parameters
+        ----------
+        key : jax.random.PRNGKey
+            The PRNG used as the random key for initialization.
+
+        *args
+            Arbitrary additional arguments for the model's
+            initialization function.
+
+        **kwargs
+            Arbitrary additional arguments for the model's
+            initialization function.
+
+        Returns
+        -------
+        StepperState
+            The new wrapped state with random initialization.
+        """
+        model_state = self.model.create_initial_state(key, *args, **kwargs)
         return self.initialize_stepper_state(model_state)
 
     def initialize_stepper_state(self, state, /):
+        """Wrap an existing state from :attr:`model` in a
+        :class:`StepperState`, preparing it for time stepping.
+
+        This function takes an existing inner model state and wraps it
+        so that it can be stepped through time by :attr:`stepper`.
+
+        This function defers to :attr:`stepper` to perform the
+        wrapping.
+
+        Parameters
+        ----------
+        state
+            The inner model state to wrap. The type depends on
+            :attr:`model` but is likely to be
+            :class:`PseudoSpectralState
+            <pyqg_jax.state.PseudoSpectralState>` or
+            :class:`ParameterizedModelState
+            <pyqg_jax.state.ParameterizedModelState>`.
+
+        Returns
+        -------
+        StepperState
+            A wrapped copy of `state`.
+        """
         return self.stepper.initialize_stepper_state(state)
 
     def step_model(self, stepper_state, /):
+        """Update a state by computing the next time step.
+
+        This method handles the interaction between :attr:`model` and
+        :attr:`stepper`, including post-processing/filtering.
+
+        To take multiple steps over time combine this method with
+        :func:`jax.lax.scan`.
+
+        Parameters
+        ----------
+        stepper_state : StepperState
+            The wrapped state to step forward in time.
+
+        Returns
+        -------
+        StepperState
+            The updated wrapped state, a new object.
+        """
         new_stepper_state = self.stepper.apply_updates(
             stepper_state,
             self.model.get_updates(stepper_state.state),
@@ -85,6 +231,25 @@ class SteppedModel:
         return new_stepper_state.update(state=postprocessed_state)
 
     def get_full_state(self, stepper_state, /):
+        """Expand a wrapped partial state into an *unwrapped* full
+        state.
+
+        This function defers to :attr:`model` to compute the full
+        state.
+
+        Parameters
+        ----------
+        stepper_state : StepperState
+            The wrapped state to be expanded.
+
+        Returns
+        -------
+        FullPseudoSpectralState
+            The expanded state. The real type depends on
+            :attr:`model`, but is likely to be
+            :class:`FullPseudoSpectralState
+            <pyqg_jax.state.FullPseudoSpectralState>`.
+        """
         return self.model.get_full_state(stepper_state.state)
 
     def _tree_flatten(self):
@@ -178,10 +343,45 @@ AB3State(
 
 @_utils.register_pytree_node_class_private
 class AB3Stepper(Stepper):
+    """Third order Adams–Bashforth stepper.
+
+    This is the same time stepping scheme as used in PyQG.
+
+    This time-stepper bootstraps using lower order Adams-Bashforth
+    schemes for the first two steps.
+
+    Parameters
+    ----------
+    dt : float
+        Numerical time step
+
+    Attributes
+    ----------
+    dt : float
+        Numerical time step
+    """
+
     def __init__(self, dt: float):
         super().__init__(dt=dt)
 
     def initialize_stepper_state(self, state: P) -> AB3State[P]:
+        """Wrap an existing `state` from a model in a
+        :class:`StepperState` to prepare it for time stepping.
+
+        This initializes a new :class:`StepperState` from a time of
+        :pycode:`0`.
+
+        Parameters
+        ----------
+        state
+            The model state to wrap.
+
+        Returns
+        -------
+        StepperState
+            The wrapped state. Note this will be a subclass of
+            :class:`StepperState` appropriate for this time stepper.
+        """
         base_state = super().initialize_stepper_state(state)
         dummy_update: P = _dummy_step_init(state)
         return AB3State(
@@ -197,6 +397,31 @@ class AB3Stepper(Stepper):
         stepper_state: AB3State[P],
         updates: P,
     ) -> AB3State[P]:
+        """Apply `updates` to the existing `stepper_state` producing
+        the next step in time.
+
+        `updates` should be provided by the model that produced
+        :attr:`StepperState.state`.
+
+        Parameters
+        ----------
+        stepper_state : StepperState
+            The time-stepper wrapped state to be updated.
+
+        updates : PseudoSpectralState or ParameterizedModelState
+            The *unwrapped* updates to apply. The actual type of
+            `updates` depends on the model being stepped.
+
+        Returns
+        -------
+        StepperState
+            The updated, wrapped state at the next time step.
+
+        Note
+        ----
+        This method does not apply post-processing to the updated
+        state.
+        """
         new_ablevel, dt1, dt2, dt3 = jax.lax.switch(
             stepper_state._ablevel,
             [
@@ -245,7 +470,30 @@ class AB3Stepper(Stepper):
 
 @_utils.register_pytree_node_class_private
 class NoStepValue(typing.Generic[P]):
-    # Marks contents to not be stepped by provided time-steppers
+    """Shields contents from the provided time-steppers.
+
+    When a time-stepper encounters a value wrapped in this class, it
+    will skip its normal stepping computations and directly use the
+    value from the updates. This allows a user to manually update an
+    auxiliary value outside the normal time-stepping.
+
+    For example, :func:`jax.random.PRNGKey` values should not be
+    time-stepped normally. Wrapping them in this class and manually
+    :func:`updating them <jax.random.split>` can accomplish this.
+
+    This class is used as part of :class:`ParameterizedModelState
+    <pyqg_jax.parameterizations.ParameterizedModelState>`.
+
+    Parameters
+    ----------
+    value : object
+        The inner value to wrap. This can be an arbitrary JAX PyTree.
+
+    Attributes
+    ----------
+    value
+        The internal, wrapped value
+    """
 
     def __init__(self, value: P, /):
         self.value = value
