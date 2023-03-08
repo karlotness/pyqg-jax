9ee14e74ceacc2a3579084e2a794c746862e05f4
Karl Otness <karl.otness@nyu.edu>
2023-03-08 00:22:26 -0500

    Add documentation for state objects.

--- docs/reference.state.rst
+++ docs/reference.state.rst
@@ -5,5 +5,13 @@ States
 
 
 .. autoclass:: PseudoSpectralState
+   :members: update
 
 .. autoclass:: FullPseudoSpectralState
+   :members: update
+
+Model instances can have their data-type precision selected as a
+constructor argument. The enumeration :class:`Precision` gives
+available options.
+
+.. autoclass:: Precision
--- src/pyqg_jax/state.py
+++ src/pyqg_jax/state.py
@@ -2,6 +2,32 @@
 # SPDX-License-Identifier: MIT
 
 
+"""Base model state objects.
+
+In `pyqg-jax` model states are separated into immutable objects to
+more closely match the rest of JAX.
+
+Models manipulate instances of :class:`PseudoSpectralState` which
+stores only the `q` component from which other model variables are
+derived.
+
+To access these variables, the states can be expanded into a
+:class:`FullPseudoSpectralState` with the remaining attributes.
+
+.. note::
+   When time-stepping in JAX (in particular with
+   :func:`jax.lax.scan`), the state objects will have leading time
+   dimensions in addition the their normal spatial dimensions. In
+   particular, JAX will store these in "`structure-of-arrays
+   <https://en.wikipedia.org/wiki/AoS_and_SoA>`__" style.
+
+   To slice into these, consider combining,
+   :func:`jax.tree_util.tree_map` with a :term:`lambda
+   <python:lambda>` or a combination of :func:`operator.itemgetter
+   <python:operator.itemgetter>` and :class:`slice <python:slice>`.
+"""
+
+
 __all__ = ["Precision", "PseudoSpectralState", "FullPseudoSpectralState"]
 
 
@@ -13,6 +39,30 @@ from . import _utils
 
 
 class Precision(enum.Enum):
+    """Enumeration for model precision levels.
+
+    When constructing a base model, use values of this enumeration to
+    select the numerical precision which should be used for the states
+    and internal calculations.
+
+    Double precision may be significantly slower, for example on GPUs.
+
+    Attributes
+    ----------
+    SINGLE
+        Single precision.
+
+        Models will use :class:`jax.numpy.float32` and :class:`jax.numpy.complex64`.
+
+    DOUBLE
+        Double precision
+
+        Models will use :class:`jax.numpy.float64` and :class:`jax.numpy.complex128`.
+
+        Ensure that JAX has `64-bit precision enabled
+        <https://jax.readthedocs.io/en/latest/notebooks/Common_Gotchas_in_JAX.html#double-64bit-precision>`__.
+    """
+
     SINGLE = enum.auto()
     DOUBLE = enum.auto()
 
@@ -28,6 +78,31 @@ def _generic_irfftn(a):
 @_utils.register_pytree_dataclass
 @dataclasses.dataclass(frozen=True)
 class PseudoSpectralState:
+    """Core state evolved by a model instance.
+
+    This is the innermost state type evolved by the models. This state
+    can be expanded into a :class:`FullPseudoSpectralState` by calling
+    methods on the models.
+
+    Warning
+    -------
+    You should not construct this class yourself. Instead, you should
+    retrieve instances from a model and if desired :meth:`update`
+    their attributes.
+
+    Attributes
+    ----------
+    q : jax.Array
+        Potential vorticity in real space.
+
+        This entry has shape :pycode:`(nz, ny, nx)`
+
+    qh : jax.Array
+        Potential vorticity in spectral space.
+
+        This is the spectral form of :attr:`q`.
+    """
+
     _: dataclasses.KW_ONLY
     qh: jnp.ndarray
 
@@ -44,6 +119,39 @@ class PseudoSpectralState:
         ...
 
     def update(self, **kwargs) -> "PseudoSpectralState":
+        """Replace the value stored in this state.
+
+        This function produces a *new* state object, containing the
+        replacement value.
+
+        The keyword arguments may be either `q` or `qh` (not both),
+        allowing the replacement value to be provided in spectral form
+        if desired.
+
+        The object this method is called on is not modified.
+
+        Parameters
+        ----------
+        q : jax.Array
+            Replacement value for :attr:`q`
+
+        qh : jax.Array
+            Replacement value for :attr:`qh`
+
+        Returns
+        -------
+        PseudoSpectralState
+            A copy of this object with the specified values replaced.
+
+        Raises
+        ------
+        ValueError
+            If the shape of the replacement does not match the
+            existing shape or if duplicate updates are specified.
+
+        TypeError
+            If the dtype of the replacement does not match the existing type.
+        """
         if len(kwargs) > 1:
             raise ValueError("duplicate updates for q (specified q and qh)")
         if "q" in kwargs:
@@ -65,6 +173,60 @@ class PseudoSpectralState:
 @_utils.register_pytree_dataclass
 @dataclasses.dataclass(frozen=True)
 class FullPseudoSpectralState:
+    """Full state including calculated values expanded by a model.
+
+    This is an expanded form of :class:`PseudoSpectralState` which
+    includes additional attributes calculated as part of running one
+    of the models.
+
+    Warning
+    -------
+    You should not construct this class yourself. Instead, you should
+    retrieve instances from a model and if desired :meth:`update`
+    their attributes.
+
+    Attributes
+    ----------
+    state : PseudoSpectralState
+        Inner, partial state providing values for :attr:`q` and
+        :attr:`qh`.
+
+    q : jax.Array
+        Potential vorticity in real space.
+
+        Pass-through accessor for :attr:`state.q <PseudoSpectralState.q>`
+
+    qh : jax.Array
+        Potential vorticity in spectral space.
+
+        Pass-through accessor for :attr:`state.qh <PseudoSpectralState.qh>`
+
+    ph : jax.Array
+        Streamfunction in spectral space.
+
+    u : jax.Array
+        Zonal velocity anomaly in real space.
+
+    uh : jax.Array
+        Zonal velocity anomaly in spectral space.
+
+    v : jax.Array
+        Meridional velocity anomaly in real space.
+
+    vh : jax.Array
+        Meridional velocity anomaly in spectral space.
+
+    uq : jax.Array
+
+    vq : jax.Array
+
+    dqhdt : jax.Array
+        Spectral derivative with respect to time for :attr:`qh`.
+
+        This value is the update applied to the model when time
+        stepping.
+    """
+
     _: dataclasses.KW_ONLY
     state: PseudoSpectralState
     ph: jnp.ndarray
@@ -103,6 +265,73 @@ class FullPseudoSpectralState:
         return _generic_rfftn(self.vq)
 
     def update(self, **kwargs) -> "FullPseudoSpectralState":
+        """Replace values stored in this state.
+
+        This function produces a *new* state object, with specified
+        attributes replaced.
+
+        The keyword arguments may specify any of this class's
+        attributes, but must not apply multiple updates to the same
+        attribute. That is, modifying both the spectral and real space
+        values at the same time is not allowed.
+
+        The object this method is called on is not modified.
+
+        Parameters
+        ----------
+        state : PseudoSpectralState
+            Replacement value for :attr:`state`
+
+        q : jax.Array
+            Replacement value for :attr:`q`. This also updates :attr:`state`.
+
+        qh : jax.Array
+            Replacement value for :attr:`qh`. This also updates :attr:`state`.
+
+        ph : jax.Array
+            Replacement value for :attr:`ph`.
+
+        u : jax.Array
+            Replacement value for :attr:`u`.
+
+        uh : jax.Array
+            Replacement value for :attr:`uh`.
+
+        v : jax.Array
+            Replacement value for :attr:`v`.
+
+        vh : jax.Array
+            Replacement value for :attr:`vh`.
+
+        uq : jax.Array
+            Replacement value for :attr:`uq`.
+
+        uqh : jax.Array
+            Replacement value for :attr:`uqh`.
+
+        vq : jax.Array
+            Replacement value for :attr:`vq`.
+
+        vqh : jax.Array
+            Replacement value for :attr:`vqh`.
+
+        dqhdt : jax.Array
+            Replacement value for :attr:`dqhdt`.
+
+        Returns
+        -------
+        FullPseudoSpectralState
+            A copy of this object with the specified values replaced.
+
+        Raises
+        ------
+        ValueError
+            If the shape of the replacement does not match the
+            existing shape or if duplicate updates are specified.
+
+        TypeError
+            If the dtype of the replacement does not match the existing type.
+        """
         new_values = {}
         if "state" in kwargs:
             raise ValueError(
